feed3ee69c3f3f1e412b424c819fb314533858da
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-09-29 03:19:33 +0300

    Add 0x00-pascal_triangle project

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,40 @@
+"""Algorithm for pascal triangle"""
+
+# Factorials dict for memoization and performance
+factorials = {}
+
+
+def fact(n):
+    """Calculates the factorial of a number"""
+    if factorials.get(n, None):
+        return factorials.get(n)
+    if n <= 1:
+        return 1
+
+    factorials[n] = n * fact(n - 1)
+    return factorials[n]
+
+
+def coefficient(n, k):
+    """Calculates the coefficient of position k with power n"""
+    return fact(n) // (fact(k) * fact(n - k))
+
+
+def coefficients(n):
+    """Returns an array of coefficients in a degree n"""
+    if n < 1:
+        return [1]
+
+    result = []
+    for i in range(n + 1):
+        result.append(coefficient(n, i))
+
+    return result
+
+
+def pascal_triangle(n):
+    result = []
+    for i in range(n):
+        result.append(coefficients(i))
+
+    return result
