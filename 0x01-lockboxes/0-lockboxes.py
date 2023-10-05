c2071b6b4703dca7ada4f3a2bd65220f19d25860
mostafaehab96 <36134234+mostafaehab96@users.noreply.github.com>
2023-10-05 09:09:17 +0300

    Add task 0x01-lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Solving lock boxes problem"""
+
+
+def look_inside(box, count):
+    """Looks inside a box and returns a list of keys that are less than the
+    specified count.
+
+    Args:
+        box: A set of keys.
+        count: The number of boxes we have.
+
+    Returns:
+        A list of keys that are less than the specified count.
+    """
+    return {key for key in box if key < count}
+
+
+def canUnlockAll(boxes: list):
+    """
+    Determines if it is possible to unlock all the boxes in a list of boxes
+    by following their contained keys.
+    :param boxes: list of lists (boxes) each box contain some keys
+    :return:
+        True if all boxes can be unlocked False otherwise
+    """
+    i = 0
+    unlocked = {0}
+
+    while i < len(unlocked):
+        keys = look_inside(boxes[i], len(boxes))
+        unlocked.update(keys)
+        i += 1
+        if len(unlocked) == len(boxes):
+            return True
+
+    return False
